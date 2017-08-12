1ea2e137b33b492b5c376138f9267a1fee92087d
MRSessions <mattsessions@live.com>
2017-08-12 13:10:18 -0500

    Cleaned Up Code and Added Method Comments

--- LTCCRM/Form1.vb
+++ LTCCRM/Form1.vb
@@ -1,6 +1,15 @@
 ﻿'Learning Team C
 
 Public Class Form1
+
+    Private Const DateFormat As String = "{0:MM/dd/yyyy}"
+
+    ''' <summary>
+    ''' Execute code upon button click
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'Hard coded test insert do not use
         'CRMDATATableAdapter.Insert("Matthew", "Sessions", 139847, "08112017", "Cash", "08112017")
@@ -10,28 +19,34 @@ Public Class Form1
 
     End Sub
 
-    Private Sub CRMDATABindingNavigatorSaveItem_Click(sender As Object, e As EventArgs)
-        Me.Validate()
-        Me.CRMDATABindingSource.EndEdit()
-        Me.TableAdapterManager.UpdateAll(Me.CRMDataSet)
-
-    End Sub
-
+    ''' <summary>
+    ''' Execute code upon form load
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'TODO: This line of code loads data into the 'CRMDataSet.CRMDATA' table. You can move, or remove it, as needed.
-        'Me.CRMDATATableAdapter.Fill(Me.CRMDataSet.CRMDATA)
 
         PreFillFields()
 
     End Sub
 
+    ''' <summary>
+    ''' This code pre fills all fields when called
+    ''' </summary>
+    ''' <remarks></remarks>
     Private Sub PreFillFields()
-        Dim todaysDate As String = String.Format("{0:MM/dd/yyyy}", DateTime.Now)
+        Dim todaysDate As String = String.Format(DateFormat, DateTime.Now)
         U_USERTextBox.Text = CRMDATATableAdapter.SelectMaxUserID() + 1
         D_LAST_PURCHASEDTextBox.Text = todaysDate
         D_START_DATETextBox.Text = todaysDate
     End Sub
 
+    ''' <summary>
+    ''' When method is called it calls the table adapter insert to insert form information
+    ''' into the database then clears fields
+    ''' </summary>
+    ''' <remarks></remarks>
     Private Sub AddCustomer()
 
         CRMDATATableAdapter.Insert(
@@ -44,14 +59,21 @@ Public Class Form1
                      )
 
         ClearFields()
+
     End Sub
 
+    ''' <summary>
+    ''' Clears all fields and calls the PreFillFields() method to fill in fields
+    ''' </summary>
+    ''' <remarks></remarks>
     Private Sub ClearFields()
+
         T_FIRST_NAMETextBox.Text = String.Empty
         T_LAST_NAMETextBox.Text = String.Empty
         U_LAST_RECEIPTTextBox.Text = String.Empty
         T_PAY_METHTextBox.SelectedIndex = -1
 
         PreFillFields()
+
     End Sub
 End Class
